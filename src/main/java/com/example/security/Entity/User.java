c6feaa3c26c20be4d94853d8b50c6918da1d5857
aashishsursa <aashish.karki@sursatech.com>
2023-11-07 13:02:36 +0545

    fix : enumType String

--- src/main/java/com/example/security/Entity/User.java
+++ src/main/java/com/example/security/Entity/User.java
@@ -32,7 +32,7 @@ public class User implements UserDetails {
     private String email;
     private String password;
 
-    @Enumerated(EnumType.ORDINAL)
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @Override
